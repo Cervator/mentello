caf065b1ee8eb1becd4c359f49b91b772ffb1096
Cervator <cervator@gmail.com>
2018-10-02 00:14:40 -0400

    Make a more super greeting

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,11 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello there, %s!";
+    private static final String template2 = "A SUPER hello there, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping("/supergreet")
+    public Greeting superGreeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template2, name));
     }
 }
